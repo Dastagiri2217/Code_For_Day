3af2db319278d1bf506209129bc927d8663d9093
Dastagiri <162585700+Dastagiri2217@users.noreply.github.com>
2026-01-20 22:00:14 +0530

    Add files via upload

---
+++ Day-12_Patterns/Pattern5.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Pattern5{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Inverted Right pyramid ");
+		System.out.println("\nEnter the n value : ");
+		int n = sc.nextInt();
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=(n-i+1);j++){
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
+	

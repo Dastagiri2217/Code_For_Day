5ac386292ad53cd9866d0d6902ce0df6ec430c39
Dastagiri <162585700+Dastagiri2217@users.noreply.github.com>
2026-01-20 21:59:45 +0530

    Add files via upload

---
+++ Day-12_Patterns/Pattern2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Pattern2{
+	public static void main(String args[]){
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Right angeled Pattern");
+		System.out.println("Enter the n value : ");
+		int n = sc.nextInt();
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=i;j++){
+				System.out.print("* ");
+			}
+		    System.out.println();
+		}
+	}
+}
+	
---
+++ Day-12_Patterns/Pattern3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Pattern3{
+	public static void main(String args[]){
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Right angeled Pattern with numbers");
+		System.out.println("\nEnter the n value : ");
+		int n = sc.nextInt();
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=i;j++){
+				System.out.print(j+" ");
+			}
+		    System.out.println();
+		}
+	}
+}
+	
